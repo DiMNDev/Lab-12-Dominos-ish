c6fc4dd9b0a0537e69bf4d97f093ee0fd7d19f45
DiMNDev <Dimndev@gmail.com>
2024-11-11 14:39:52 -0700

    added player class

--- lab-12.Library/Class1.cs
+++ lab-12.Library/Class1.cs
@@ -51,3 +51,21 @@ public class Tile : IEquatable<Tile?>
 
     public override string ToString() => $"[{Num1}|{Num2}]";
 }
+
+public class Player
+{
+    public const int StartingTileCount = 7;
+    private readonly string _name;
+    public string Name => _name;
+
+    public List<Tile> Tiles { get; private set; } = new();
+
+    public Player(string name, int tileCount = StartingTileCount)
+    {
+        _name = name;
+        Tiles.AddRange(Enumerable.Range(0, tileCount).Select(_ => new Tile()));
+    }
+
+    internal bool HasMatchFor(Tile other) =>
+       Tiles.Any(t => t.Num1 == other.Num2 || t.Num2 == other.Num2);
+}
