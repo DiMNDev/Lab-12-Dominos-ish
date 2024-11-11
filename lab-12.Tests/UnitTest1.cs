3d2959a59e9b9bbf3ff6c4c40086def260e7f615
DiMNDev <Dimndev@gmail.com>
2024-11-11 14:38:11 -0700

    Added Tile class / equality test

--- lab-12.Tests/UnitTest1.cs
+++ lab-12.Tests/UnitTest1.cs
@@ -1,10 +1,13 @@
 namespace lab_12.Tests;
+using Library;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void OppositeTilesAreEqual()
     {
-
+        var t1 = new Tile(1, 2);
+        var t2 = new Tile(2, 1);
+        t1.Equals(t2);
     }
 }
